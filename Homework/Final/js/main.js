6c65fae359001f766a0f89b778f3fcf41ab9ed89
Your Name HereMichael <mfort657@mail.endicott.edu>
2013-02-28 09:11:07 -0500

    JSON update

--- Homework/Final/js/main.js
+++ Homework/Final/js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function(){
+	$.getJSON('http://api.espn.com/v1/now?apikey=wz9xgsmyzk5dx3jxsudq62bs', function(data){
+    $.each(data.feed, function(i, item){
+      var headline = item.headline,
+          web_url = item.links.web.href,
+          description = item.description,
+          news_item = "<li>The Headline is <a href='" + web_url + "'>" + headline + "</a><p>" + description + "</p></li>";
 
+      console.log("The Headline is " + headline); //use to test
+      $('#espn_news ul').append(news_item);
+    }) // end each
+  }) //JSON
+}); //jQuery
