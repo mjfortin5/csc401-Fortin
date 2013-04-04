884bec6a4d5604a1d32ea20dccacd3c12071880b
Your Name HereMichael <mfort657@mail.endicott.edu>
2013-04-04 08:43:29 -0400

    Blog update

---
+++ Homework/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ac3a0f63e9078784eacb4b5416daedd75104bffb07063352366314ab9a72b23e6b6225ee245fc7f139133c0dd4fbbd626c72243d12e3b27dd5dedf7942aec8b5'
